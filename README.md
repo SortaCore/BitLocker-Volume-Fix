71cc7d73a9d444a7d045b4891a8bf2d8af042b78
Phi <phi@dark-wire.com>
2020-11-28 20:53:29 +0000

    Real commit
    First setup.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # BitLocker-Volume-Fix
- When "parameter is incorrect" bugs occur.
+When "parameter is incorrect" bugs occur when opening a BitLocker drive, after unlocking the drive
+with the correct password, this script can be used to correctly mount it.
+
+## How to run the script
+Edit lines 12-15 before running!  
+And run it with admin permissions.  
+(If you're using scheduled tasks to do it on bootup, set up your user
+as login, and check the "Run with highest priviledges" box.)
+
+The "interim letter" should be a letter you're not ever using; as part of the fix,
+the VHDX will take this letter temporarily in the middle of the process. After unlocking,
+the VHDX will have the "end letter" letter.
+
+Also note the text files included with this script are necessary. Some more text files will be generated.
+
+## If it doesn't work anymore
+In the event this script no longer works, and you still get a "parameter is incorrect" error,
+change the interim letter you're using in line 15.
+
+You may also want to go into the registry and delete any mention of the original interim letter,
+as that usually gets things working again.
+
+## How to do it manually
+Steps the script performs for those wanting a manual Disk Management way:
+1. Attach the drive.
+2. Remove all drive letters it uses. No need to unlock BitLocker.
+3. Detach the drive.
+4. Attach the drive again. It should have no drive letters.
+5. Add a different drive letter, not the one you intend to use.
+6. Detach the drive again.
+7. Attach the drive again.
+8. Unlock the drive with its different letter, using BitLocker password input.
+9. Change drive letter back to what it should be.
+10. Drive is now unlocked properly.
+
+Note that these steps will need repeating every time the computer starts up, so maybe use a scheduled task instead
+ - remember to run it as your user, and check the highest priviledges box.
+
+## Extra
+This script will also have some safeguards for startup under midway points, e.g. running it while drive is already mounted, etc.
+This script will use the TXT files in the same folder. It will also generate more TXT files.
---
+++ Unlock Drive.bat
@@ -0,0 +1,143 @@
+@echo off
+rem This uses the fix where drive letter is swapped to another letter and back.
+rem Uses letter %interimLetter%:\ as interim letter. Uses %endLetter%:\ as expected drive letter.
+rem Uses %~dp0BitLBackup.vhdx as file to replace.
+rem 
+rem Requires user to input password. (The BDEUnlock command does allow unlocking from file,
+rem but this makes security pointless.)
+rem Requires admin command prompt.
+rem
+rem Volume numbers are 0-based. Worth noting subst'd stuff like X:\ do not show up.
+rem 
+rem SET THESE BEFORE RUNNING
+set "interimLetter=P"
+set "endLetter=Z"
+set "vhdPath=%~dp0BitLBackup.vhdx"
+
+echo Opening "%vhdPath%" as %endLetter%:\ (interim letter %interimLetter%:)...
+rem Check for admin permissions
+net session >nul 2>&1
+if not errorLevel 0 (
+	echo FATAL ERROR: Not running as admin. DiskPart requires admin permissions.
+	pause
+	exit
+)
+
+pause
+rem Check no %interimLetter%\ drive connected, if so, ask to continue and remove the letter
+echo select volume %interimLetter%: | diskpart | find "The volume you selected is not valid or does not exist." > nul
+rem errorlevel 0 means found it
+IF not errorlevel 0 (
+	rem TODO: Currently unsafe, as %interimLetter%:\ may not be virtual disk file we expect it to be. Prompt first.
+	echo %interimLetter%:\ drive mounted at start.
+	choice /C YN /N /M "Press Y to REMOVE %interimLetter%: DRIVE LETTER from whatever it's attached to [Y/N]? "
+	if errorlevel 2 (
+		pause
+		exit
+	)
+	echo "Removing %interimLetter% drive letter..." 
+	(
+	echo select volume=%interimLetter%:
+	echo remove letter=%interimLetter%
+	) | diskpart > nul
+	echo Done, waiting for 15 seconds as per docs...
+	timeout /T 15 /NOBREAK > nul
+)
+
+rem Check virtual disk is not already connected
+rem note that a disk that is detached may still show up as status "Added", type "Unknown"
+rem When actually attached, status "Attached, not open" and type "Expandable"
+(echo list vdisk | diskpart | findstr /C:"%vhdPath%" | findstr "Expandable") > nul
+rem errorlevel 0 means found it
+IF errorlevel 0 (
+	echo Warning: Disk already attached. Performing dismount.
+	choice /C YN /N /M "Press Y to continue [Y/N]? "
+	if errorlevel 2 (
+		pause
+		exit
+	)
+	echo Dismounting...
+	(
+	echo select vdisk file=VHDPATH
+	echo detach vdisk
+	) | diskpart > nul
+	echo Waiting for 15 seconds as per docs...
+	timeout /T 15 /NOBREAK > nul
+)
+
+rem Generate unlock/lock scripts for BitLocker.
+SETLOCAL EnableDelayedExpansion
+rem Check no %endLetter%:\ drive connected
+echo select volume %endLetter%: | diskpart | find "The volume you selected is not valid or does not exist."
+rem errorlevel 0 means found it
+IF not errorlevel 0 (
+	echo FATAL ERROR
+	echo %endLetter%:\ drive mounted at start. Dismount it, and run script again.
+	pause
+	exit
+)
+
+echo list volume | diskpart > "%~dp0diskout.txt"
+rem If there's 10 volumes, they will be numbered 0-9.
+rem findstr finds 10 matches, and new volume index is 10, so no reason to increment.
+set volumeNum=
+(findstr /R /C:"Volume [0-9][0-9 ] " "%~dp0diskout.txt" | find /C " ") > "%~dp0volumenum.txt"
+for /F "delims=" %%A in (%~dp0\volumenum.txt) do set volumeNum=%%A
+
+rem empty file, then append all lines with NEWVOLINDEX replaced
+break> %~dp0DiskPart_PreUnlockDynamic.txt
+break> %~dp0DiskPart_PostUnlockDynamic.txt
+for /F "delims=" %%A in (%~dp0DiskPart_PreUnlockTemplate.txt) do (
+	set "string=%%A"
+	set "modified=!string:NEWVOLINDEX=%volumeNum%!"
+	set "string=!modified!"
+	set "modified=!string:ENDLETTER=%endLetter%!"
+	set "string=!modified!"
+	set "modified=!string:VHDPATH=%vhdPath%!"
+	echo !modified!>> %~dp0DiskPart_PreUnlockDynamic.txt
+)
+for /F "delims=" %%A in (%~dp0DiskPart_PostUnlockTemplate.txt) do (
+	set "string=%%A"
+	set "modified=!string:NEWVOLINDEX=%volumeNum%!"
+	set "string=!modified!"
+	set "modified=!string:INTERIMLETTER=%interimLetter%!"
+	set "string=!modified!"
+	set "modified=!string:ENDLETTER=%endLetter%!"
+	set "string=!modified!"
+	set "modified=!string:VHDPATH=%vhdPath%!"
+	echo !modified!>> %~dp0DiskPart_PostUnlockDynamic.txt
+)
+
+start "Mounting encrypted drive..." "C:\Windows\System32\diskpart.exe" /s "%~dp0DiskPart_PreUnlockDynamic.txt" > nul
+
+echo Completed mount, still drive unlocking to do.
+echo Press a key to open password prompt...
+pause
+rem Any services that use %endLetter%:\ drive you might want to stop here.
+
+endlocal
+:userUnlock
+echo Running unlock drive, please enter password...
+rem The drive is unlocked on letter %interimLetter%:, then rewritten to %endLetter%:
+start "Waiting for unlock" /WAIT bdeunlock %interimLetter%: > nul
+echo Expected decrypted, waiting for 15 seconds as per docs...
+
+timeout /T 15 /NOBREAK > nul
+rem Should be decrypted now. If it is, the filesystem will show up valid, otherwise as Unkno[wn]
+(echo list volume | diskpart | findstr /C:"Volume %volumeNum%" | findstr "Unkno") > nul
+rem errorlevel 0 means found it, so if not found
+IF errorlevel 0 (
+	choice /C YN /N /M "Did not decrypt properly. [Y/N]? "
+	if errorlevel 2 (
+		pause
+		exit
+	)
+	goto userUnlock
+)
+
+start "Reassigning encrypted drive letter..." /WAIT "C:\Windows\System32\diskpart.exe" /s "%~dp0DiskPart_PostUnlockDynamic.txt" > nul
+
+rem Any services that use %endLetter%:\ drive you might want to restart here.
+echo Completed.
+pause
+exit
